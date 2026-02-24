07dec34b2196117cae12fd28d2a43f387d973bb4
Jessicaaa05 <leejessica0110@gmail.com>
2026-02-24 20:05:19 +0800

    Admin routing setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'firebase_options.dart';
+
 import 'Admin/home.dart';
 import 'Admin/dumping_stations.dart';
 import 'Admin/reports.dart';
@@ -14,6 +17,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kIsWeb) {
+    setUrlStrategy(PathUrlStrategy());
+  }
+
   runApp(const MyApp());
 }
 
@@ -24,13 +32,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TaskHomePage(),
+      initialRoute: '/company',
       routes: {
         '/company': (context) => const AdminHomePage(),
-        '/company/dumping-stations': (context) => const DumpingStationsPage(),//
-        '/company/reports': (context) => const ReportsPage(),//
-        '/company/waste-profile': (context) => const WasteProfilePage(),//
-        '/company/summary-dashboard': (context) => const SummaryDashboardPage(),//
+        '/company/dumping-stations': (context) => const DumpingStationsPage(),
+        '/company/reports': (context) => const ReportsPage(),
+        '/company/waste-profile': (context) => const WasteProfilePage(),
+        '/company/summary-dashboard': (context) => const SummaryDashboardPage(),
         '/company/locations': (context) => const LocationsPage(),
       },
     );
